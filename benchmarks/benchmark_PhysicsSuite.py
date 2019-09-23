39c1914bdb79d45f5e28d45e6e593f4140cdaf28
diego diaz <diego7319@gmail.com>
2019-09-23 00:16:36 -0500

    WIP bench physics suite

--- benchmarks/benchmark_PhysicsSuite.py
+++ benchmarks/benchmark_PhysicsSuite.py
@@ -1,13 +1,34 @@
+"""Functions from Physics to benchmark with aerospeed velocity """
+
 import astropy.constants as const
 import astropy.units as u
+from numpy import pi
+from plasmapy.physics import (
+    cold_plasma_permittivity_SDP,
+    cold_plasma_permittivity_LRP
+)
 
-
-class PhysicsSuite:
+class Dielectric:
     """
-    An example benchmark that times the performance of various kinds
-    of iterating over dictionaries in Python.
+    Benchmark that times the performance of funcions from
+    Physics/dielectric package
     """
+    B = 2*u.T
+    species = ['e', 'D+']
+    n = [1e18*u.m**-3, 1e18*u.m**-3]
+    omega = 3.7e9*(2*pi)*(u.rad/u.s)
     def setup(self):
-        self.d = {}
-        for x in range(500):
-            self.d[x] = None
+        pass
+
+    def time_cold_plasma_permittivity_SDP(self):
+        permittivity = S, D, P = cold_plasma_permittivity_SDP(
+                                                             Dielectric.B,
+                                                             Dielectric.species,
+                                                             Dielectric.n,
+                                                             Dielectric.omega)
+    def time_cold_plasma_permittivity_LRP(self):
+        permittivity = S, D, P = cold_plasma_permittivity_LRP(
+                                                             Dielectric.B,
+                                                             Dielectric.species,
+                                                             Dielectric.n,
+                                                             Dielectric.omega)
