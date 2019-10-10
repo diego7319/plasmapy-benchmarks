57b88c37ace3daab0fa6dd3deffd9d870393a0c0
diego7319 <diego7319@users.noreply.github.com>
2019-10-10 00:13:27 -0500

    New benchmarks for other functions under physics and mathematics (#4)
    * new benchmarks for physics and mathematics
    
    * change in python version asv.conf.json
    
    * asv.conf.json changed to only python 3.7
    
    * fixed imports physicsSuite
    
    * fixed function name

---
+++ benchmarks/benchmark_MathematicsSuite.py
@@ -0,0 +1,27 @@
+"""Functions from mathematics to benchmark with aerospeed velocity """
+
+import astropy.units as u
+from plasmapy.mathematics import (
+    plasma_dispersion_func,
+    plasma_dispersion_func_deriv,
+    Fermi_integral
+)
+
+
+class mathematics:
+    """
+    Benchmark that times the performance of funcions from
+    mathematics/mathematics.py file
+    """
+
+    def setup(self):
+        pass
+
+    def time_plasma_dispersion_func(self):
+        plasma_dispersion_func(-1.52+0.47j)
+
+    def time_plasma_dispersion_func_deriv(self):
+        plasma_dispersion_func_deriv(-1.52+0.47j)
+
+    def time_Fermi_integral(self):
+        Fermi_integral(1, 0)
